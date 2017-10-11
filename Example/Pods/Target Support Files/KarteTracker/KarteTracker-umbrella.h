e6ded740f5755d6db80ea894b1dab6707c29fa99
makinoy <makino.y@gmail.com>
2017-10-11 18:17:00 +0900

    Update pods

--- Example/Pods/Target Support Files/KarteTracker/KarteTracker-umbrella.h
+++ Example/Pods/Target Support Files/KarteTracker/KarteTracker-umbrella.h
@@ -1,11 +1,12 @@
+#ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#endif
 
 #import "KarteTracker.h"
 #import "KarteTrackerAppProfile.h"
 #import "KarteTrackerConfig.h"
 #import "KarteTrackerUserProfile.h"
 #import "KarteTrackerUtil.h"
-#import "NSData+ZlibDeflate.h"
 
 FOUNDATION_EXPORT double KarteTrackerVersionNumber;
 FOUNDATION_EXPORT const unsigned char KarteTrackerVersionString[];
